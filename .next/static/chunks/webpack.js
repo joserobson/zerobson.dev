8e092016957090965e9a3784d2610d6d07c68e9c
José Robson <robsbq@gmail.com>
2025-07-23 14:59:21 -0300

    Updated app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b8c73f392972428a"; }
+/******/ 		__webpack_require__.h = function() { return "3807cc339493286d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
