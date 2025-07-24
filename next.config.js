a89f77e6b6668a2d6ae93469a33827f69a327c00
José Robson <robsbq@gmail.com>
2025-07-23 23:46:55 -0300

    Fix next-intl configuration path

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const createNextIntlPlugin = require('next-intl/plugin');
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
