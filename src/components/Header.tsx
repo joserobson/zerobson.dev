769f0b130e149019ab1bb8dcc4f751bbbe7a51f7
José Robson <robsbq@gmail.com>
2025-07-25 17:03:59 -0300

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,7 +21,7 @@ export default function Header() {
         <div className="flex justify-between h-16">
           <div className="flex items-center">
             <Link href="/" className="text-xl font-bold text-gray-900">
-              Your Name
+              José Robson
             </Link>
           </div>
 
